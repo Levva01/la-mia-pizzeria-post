3775c5e89e03e224d7d4b740cdbf5885f10ab4d0
Levva01 <vellanifederico13@gmail.com>
2022-10-05 14:55:13 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_post.Models;
+
+namespace la_mia_pizzeria_post.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly List<Pizza> _pizze = new()
+    {
+        new Pizza
+        {
+            Id = 1,
+            Nome = "Margherita",
+            Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+            Foto = "img/pizza.jpg",
+            Prezzo = 5.00
+        },
+
+        new Pizza
+        {
+            Id = 2,
+            Nome = "Margherita",
+            Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+            Foto = "img/pizza.jpg",
+            Prezzo = 5.00
+        },
+
+        new Pizza
+        {
+            Id = 3,
+            Nome = "Margherita",
+            Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+            Foto = "img/pizza.jpg",
+            Prezzo = 5.00
+        },
+
+        new Pizza
+        {
+            Id = 4,
+            Nome = "Margherita",
+            Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+            Foto = "img/pizza.jpg",
+            Prezzo = 5.00
+        },
+
+        new Pizza
+        {
+            Id = 5,
+            Nome = "Margherita",
+            Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+            Foto = "img/pizza.jpg",
+            Prezzo = 5.00
+        },
+    };
+        public IActionResult Index()
+        {
+            return View(_pizze);
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,11 @@
+﻿namespace la_mia_pizzeria_post.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Foto { get; set; }
+        public double Prezzo { get; set; }
+    }
+}
