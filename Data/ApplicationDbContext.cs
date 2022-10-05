b7c54b745691cbba94cac41514ad575d37dadc54
Levva01 <vellanifederico13@gmail.com>
2022-10-05 15:32:26 +0200

    Migrations done

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using la_mia_pizzeria_post.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Pizza> Pizze { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db_pizzeria;Integrated Security=True");
+    }
+
+}
